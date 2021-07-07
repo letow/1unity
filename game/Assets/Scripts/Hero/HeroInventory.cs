bc5b637fb7dc4bc0197d1b245a6c4d37ef5d8c16
George <71167099+letow@users.noreply.github.com>
2021-07-08 01:16:31 +0500

    Adding inventory

---
+++ game/Assets/Scripts/Hero/HeroInventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroInventory : MonoBehaviour
+{
+    public List<Item> item = new List<Item>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ game/Assets/Scripts/Object/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+	public string nameItem;
+	public string typeItem;
+	public int price;
+	public string pathSprite;
+	public string pathPrefab;
+}
