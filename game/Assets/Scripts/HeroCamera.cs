73e05500076e80daa77658a077bc9daa68164051
Kek-jpg <Kek-jpg@users.noreply.github.com>
2021-07-07 20:37:03 +0500

    Create HeroCamera.cs

---
+++ game/Assets/Scripts/HeroCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroCamera : MonoBehaviour
+{
+
+    public Transform target;
+    public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
+
+    public float pitch = 2f;
+    public float yawSpeed = 100f;
+
+    public float currentZoom = 10f;
+    public float currentYaw = 0;
+
+    void Update()
+    {
+        currentZoom = Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horisontal") * yawSpeed * Time.deltaTime;
+
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+    }
+}
