27355908ca9ff41a0510e55649c02bde61a0dcdd
George <71167099+letow@users.noreply.github.com>
2021-07-08 23:04:10 +0500

    Added adding items to inventory

---
+++ game/Assets/Scripts/Drag.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Drag : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+	public HeroInventory heroInventory;
+	public Item item;
+	public string ownerItem;
+	public int countItem;
+	
+	public Image image;
+	public Sprite defaultSprite;
+	public Text count;
+	
+	public Text descriptionText;
+	
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		
+	}
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		
+	}
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		if(eventData.button == PointerEventData.InputButton.Right)
+		{
+			heroInventory.RemoveItem(this);
+		}
+		else if (eventData.button == PointerEventData.InputButton.Left)
+		{
+			heroInventory.UseItem(this);
+		}
+	}
+	
+	public void RemoveCell()
+	{
+		item = null;
+		image.sprite = null;
+		//descriptionText.text = "";
+		//count.text = "";
+		ownerItem = "";
+	}
+}
--- game/Assets/Scripts/Hero/HeroInventory.cs
+++ game/Assets/Scripts/Hero/HeroInventory.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class HeroInventory : MonoBehaviour
 {
     public List<Item> item = new List<Item>();
+	public List<Drag> drag;
+	public GameObject inventory;
+	
     void Start()
     {
         
@@ -13,6 +16,51 @@ public class HeroInventory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        InventoryActive();
     }
+	
+	public void InventoryActive()
+	{
+		if(Input.GetKeyDown(KeyCode.I))
+		{
+			if(inventory.activeSelf)
+			{
+				InventoryDisable();
+			}
+			else
+			{
+				InventoryEnable();
+			}
+		}
+	}
+	public void InventoryDisable()
+	{
+		foreach (Drag drag in drag)
+			drag.RemoveCell();
+		inventory.SetActive(false);
+		
+	}
+	public void InventoryEnable()
+	{
+		inventory.SetActive(true);
+		
+		foreach(Drag drag in drag)
+			drag.RemoveCell();
+	
+		for(int i = 0; i < item.Count; i++)
+		{
+			drag[i].item = item[i];
+			drag[i].image.sprite = Resources.Load<Sprite>(item[i].pathSprite);
+			drag[i].ownerItem = "myItem";
+		}
+	}
+	
+	public void RemoveItem(Drag drag)
+	{
+		print("remove");
+	}
+	public void UseItem(Drag drag)
+	{
+		print("use");
+	}
 }
